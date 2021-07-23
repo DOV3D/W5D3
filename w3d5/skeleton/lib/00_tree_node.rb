15deae1725565c40c66455487b5edbf6da3f94e8
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-07-23 13:56:26 -0700

    reassignment completed '

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value 
+
+    def initialize(value)
+        @value = value
+        @parent = nil 
+        @children = [] 
+    end 
+
+    def parent=(node=nil)
+        if @parent != nil 
+            @parent.children.delete(self)
+        end 
+        
+        @parent = node
+
+
+        if node != nil 
+            node.children << self 
+        end  
+        
+
+    end 
+
+
+    
+
+
+
 end
