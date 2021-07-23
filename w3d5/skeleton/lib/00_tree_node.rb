2b6120073a9a5a7833c20c9b03cba6648d99de83
Josh Kim-Biggs <joshgykim@gmail.com>
2021-07-23 15:14:43 -0700

    Finished PolyTreeNode

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class PolyTreeNode
 
     attr_reader :parent, :children, :value 
@@ -12,20 +14,48 @@ class PolyTreeNode
         if @parent != nil 
             @parent.children.delete(self)
         end 
-        
-        @parent = node
 
+        @parent = node
 
         if node != nil 
             node.children << self 
         end  
-        
-
     end 
 
+    def add_child(child_node)
+        child_node.parent=(self)
+    end
 
-    
+    def remove_child(child_node)
+        raise "Node is not a child!" if !@children.include?(child_node)
+        child_node.parent = nil
+    end
 
+    def dfs(target_value)
+        # debugger
+        return self if self.value == target_value
 
+        @children.each do |child|
+            result = child.dfs(target_value)
+            if result != nil
+                return result
+            end
+        end
+        nil
+    end
+
+    def bfs(target_value) #d
+        new_a = []
+        new_a.push(self) 
+        until new_a.empty?
+            shifted_node = new_a.shift # c
+            if shifted_node.value == target_value
+                return shifted_node
+            else
+                new_a += shifted_node.children
+            end
+        end
+        nil
+    end
 
 end
