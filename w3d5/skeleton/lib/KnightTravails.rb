0d3c2a64113fd96a938ae81ea7eb5cdc9bd59a04
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-07-26 11:23:39 -0700

    working on build move tree

--- w3d5/skeleton/lib/KnightTravails.rb
+++ w3d5/skeleton/lib/KnightTravails.rb
@@ -11,17 +11,19 @@ class KnightPathFinder
 
     # Based on the pos argument
     # Returns an array of positions that would not be off-board (in a 8x8 grid)
-    def self.valid_moves(current_pos) # [3, 4]
+    require "byebug"
+    def self.valid_moves(current_node) # [3, 4]
+        #debugger
         moves = [[2, 1], [2, -1], [-2, 1], [-2, -1],
                  [1, 2], [1, -2], [-1, 2], [-1, -2]]
-        new_position = []
+        new_nodes = []
         # i = 0
         # while i < 8
         #     (1..2).each do |x|
                 
         moves.each do |move|
-            current_row = current_pos[0]
-            current_col = current_pos[1]
+            current_row = current_node.value[0]
+            current_col = current_node.value[1]
             move_row = move[0]
             move_col = move[1]
 
@@ -29,34 +31,60 @@ class KnightPathFinder
             new_col = current_col + move_col
 
             if new_row <= 7 && new_row >= 0 && new_col <= 7 && new_col >= 0
-                new_position << [new_row, new_col]
+                new_node = PolyTreeNode.new([new_row, new_col])
+                new_node.parent= (current_node)
+                new_nodes << new_node
             end
         end
+        
 
-        new_position
+        new_nodes
         
     end 
+    
 
 
     # new_move_positions
     # 1. Filters out positions that have already been visited (@considered_positions)
     # 2. Adds new moves to visited (@considered_positions)
     # 3. return new moves
-    def new_move_positions(current_pos)
-        new_positions = KnightPathFinder.valid_moves(current_pos)
+    def new_move_positions(current_node)
+        new_nodes = KnightPathFinder.valid_moves(current_pos)
 
-        new_positions.select! { |pos| !@considered_positions.include?(pos) }
+        new_nodes.select! { |node| !@considered_positions.include?(node) }
         
-        new_positions.each do |x|
-            new_knight = PolyTreeNode.new(x)
-            @considered_positions << new_knight
+        new_nodes.each do |x|
+            @considered_positions << x
         end
 
-        new_positions
+        new_nodes
     end 
 
 end 
 
-my_knight = KnightPathFinder.new([3,4])
-p my_knight.new_move_positions([3,4])
-p my_knight.considered_positions
+def build_move_tree(target)
+    queue = [root_node]
+
+    until queue.empty?
+        node = queue.first
+        if target == node 
+            
+            # return an array of nodes that lead to target 
+        else 
+           new_nodes = new_move_positions(node)
+           queue += new_nodes
+           queue.shift 
+        end 
+    end 
+
+
+end 
+
+#my_knight = KnightPathFinder.new([3,4])
+# current_node = PolyTreeNode.new([2,2])
+# p KnightPathFinder.valid_moves(current_node)
+
+
+# my_knight = KnightPathFinder.new([3,4])
+# p my_knight.new_move_positions([3,4])
+# p my_knight.considered_positions
