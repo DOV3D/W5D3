89af335cd7cf48bb77b202aca47295c349569025
Mauricio <mjlomeli@uci.edu>
2021-07-29 16:02:22 -0700

    finished rspec and started tower of hanoi

--- w4d4/lib/towers_of_hanoi.rb
+++ w4d4/lib/towers_of_hanoi.rb
@@ -4,13 +4,45 @@ class TowersofHanoi
 
     def initialize 
         @left = (1..6).to_a 
-        @mid = []
-        @right = []
+        @mid = Array.new(6){" "}
+        @right = Array.new(6){" "}
 
     end 
 
+    def win?
+        if @right == (1..6).to_a && @mid.empty? && @left.empty?
+            puts "Nice job!"
+            return true
+        end
+        false
+    end
+
+    def get_input
+        puts "Enter the start pile and end pile location like 'left, right'"
+        begin
+            input = gets.chomp.split(", ")
+        rescue !["left", "right", "mid"].include?(input)
+            puts "That is not a valid input"
+        end
+        return input
+    end
+
+    def play_turn
+        while !win?
+            puts self
+            from, to = get_input
+            place_disc(from, to)
+        end
+    end
+
+    def place_disc(from, to)
+        hash = {"left" => @left, "mid" => @mid, "right" => @right}
+        hash[to].unshift(hash[from].shift)
+    end
+
+
     def to_s 
-        system("clear")
+        #system("clear")
         rows = @left.zip(mid, right)
         rows.each do |left, mid, right|
             print "#{left}".center(10, " ")
@@ -21,7 +53,6 @@ class TowersofHanoi
         print "left".center(10, " ")
         print "mid".center(10, " ")
         puts "right".center(10, " ")
-
     end 
 
 
