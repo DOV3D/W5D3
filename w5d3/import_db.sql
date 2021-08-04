815b2e78db470ea6070fc923b278efea67abc783
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-08-04 15:26:54 -0700

    finished tables

--- w5d3/import_db.sql
+++ w5d3/import_db.sql
@@ -1,5 +1,12 @@
+DROP TABLE IF EXISTS question_follows; 
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions; 
+DROP TABLE IF EXISTS users;
+
 
 PRAGMA foreign_keys = ON;
+
 
 CREATE TABLE users(
     user_id INTEGER PRIMARY KEY,
@@ -8,7 +15,7 @@ CREATE TABLE users(
 );
 
 CREATE TABLE questions(
-    question_id INTEGER PRIMARY KEY
+    question_id INTEGER PRIMARY KEY,
     title TEXT NOT NULL, 
     body TEXT NOT NULL,
     author_id TEXT NOT NULL, 
@@ -22,19 +29,18 @@ CREATE TABLE question_follows(
     FOREIGN KEY (follower_id) REFERENCES users(user_id),
     FOREIGN KEY (asked_question_id) REFERENCES questions(question_id)
 
-
 );
 
 CREATE TABLE replies(
     reply_id INTEGER PRIMARY KEY,
     body TEXT NOT NULL, 
     question_id INTEGER NOT NULL,
-    parent_reply INTEGER NOT NULL,
+    parent_reply INTEGER,
     replier_id INTEGER NOT NULL, 
 
     FOREIGN KEY(question_id) REFERENCES questions(question_id),
     FOREIGN KEY(parent_reply) REFERENCES replies(reply_id),
-    FOREIGN KEY(replier_id) REFERENCES users(user_id),
+    FOREIGN KEY(replier_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE question_likes(
@@ -57,22 +63,22 @@ VALUES
 INSERT INTO
     questions (question_id, title, body, author_id)
 VALUES
-    (1, 'City Geographies', 'How far is SF from LA?!?', 1)
-    (2, 'City Geographies', 'How far is SF from LA?!?', 2)
-    (3, 'Wellness', 'Have you had COVID in the last 14 days?', 3)
+    (1, 'City Geographies', 'How far is SF from LA?!?', 1),
+    (2, 'City Geographies', 'How far is SF from LA?!?', 2),
+    (3, 'Wellness', 'Have you had COVID in the last 14 days?', 3);
 
 INSERT INTO
     question_follows (pair_id, follower_id, asked_question_id)
 VALUES
-    (1, 1, 3)
-    (2, 2, 3)
+    (1, 1, 3),
+    (2, 2, 3),
     (3, 3, 1);
 
 INSERT INTO
     replies (reply_id, body, question_id, parent_reply, replier_id)
 VALUES
-    (1, 'Seven hours and lots of coffee', 1, 0, 3)
-    (2, 'A billion years idk', 2, 0, 2)
+    (1, 'Seven hours and lots of coffee', 1, NULL, 3),
+    (2, 'A billion years idk', 2, 1, 2),
     (3, 'Enough time to code', 1, 1, 3);
 
 
