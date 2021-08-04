7ede062f8863e1cbdd196a3ee4f9ccf58264f52d
jayzizzle <dj.jonzamora@gmail.com>
2021-08-03 17:07:58 -0700

    solved more problems

--- w5d2/skeleton 9/lib/04_select_within_select.rb
+++ w5d2/skeleton 9/lib/04_select_within_select.rb
@@ -96,6 +96,26 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE
+      population > (
+        SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Canada'
+      ) AND population < (
+        SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Poland'
+      );
   SQL
 end
 
@@ -105,5 +125,20 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT
+    continent
+  FROM
+    countries
+  GROUP BY
+    continent
+  HAVING
+    population < 25000000;
   SQL
 end
+
+    # SELECT
+    #   DISTINCT name
+    # FROM
+    #   countries
+    # WHERE
+    #   population < 25000000);
