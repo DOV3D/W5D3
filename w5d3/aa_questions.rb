9c14bd6b772230938bfb56bea4e940ebe02a0377
App Academy Student <student@appacademy.io>
2021-08-04 10:58:09 -0700

    Interview practice

--- w5d3/aa_questions.rb
+++ w5d3/aa_questions.rb
@@ -9,3 +9,39 @@
 
 # Input: "cbbd"
 # Output: "bb"
+
+def longest_palindrome(string)
+  largest = ''
+  (0...string.length).each do |i|
+    (i + 1...string.length).each do |j|
+      sub_str = string[i..j]
+      if sub_str == sub_str.reverse
+        if sub_str.length > largest.length
+          largest = sub_str
+        end
+      end
+    end
+  end 
+  largest
+end
+
+def longest_opt(string)
+  largest = ''
+  temp = ''
+  (0...string.length).each do |i|
+    temp += string[i]
+    if temp == temp.reverse && temp.length > 1
+      if temp.length > largest.length 
+        largest = temp
+        temp = ''
+      end
+    end
+  end
+  largest
+end
+
+
+p longest_opt('babad') #'bab'
+p longest_opt('cbbd') #'bb'
+
+
