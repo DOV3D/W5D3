d977bb68febc7cf7073cce4ae3f95ccbdae61c04
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-08-04 14:18:59 -0700

    question likes completed

--- w5d3/import_db.sql
+++ w5d3/import_db.sql
@@ -2,45 +2,91 @@
 create table users(
     user_id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
-    -- user_id INTEGER NOT NULL
-    -- FOREIGN KEY (user_id) REFERENCES questions(id) 
+    lname TEXT NOT NULL
 );
 
 create table questions(
     question_id INTEGER PRIMARY KEY
     title TEXT NOT NULL, 
-    body TEXT NOT NULL, 
+    body TEXT NOT NULL,
+    author_id TEXT NOT NULL, 
+    FOREIGN KEY (author_id) REFERENCES users(user_id)
 );
 
 create table question_follows(
     pair_id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    question_id INTEGER NOT NULL
+    follower_id INTEGER NOT NULL,
+    asked_question_id INTEGER NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users(user_id),
+    FOREIGN KEY (asked_question_id) REFERENCES questions(question_id)
+
+
+);
+
+create table replies(
+    reply_id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL, 
+    question_id INTEGER NOT NULL,
+    parent_reply INTEGER NOT NULL,
+    replier_id INTEGER NOT NULL, 
+
+    FOREIGN KEY(question_id) REFERENCES questions(question_id),
+    FOREIGN KEY(parent_reply) REFERENCES replies(reply_id),
+    FOREIGN KEY(replier_id) REFERENCES users(user_id),
+);
+
+create table question_likes(
+    likes_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL, 
+    question_id INTEGER NOT NULL, 
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
+
 );
 
 
 users
 
-id    |    fname     |       lname   
+user_id    |    fname     |       lname   
 
-1       Snigdha         Banda               
-2       App             Academy             
-3       Joey            Fernandez                 
+1               Snigdha         Banda               
+2               App             Academy             
+3               Joey            Fernandez                 
 
 
 questions
 
-id     |      title      |       body        |  user_id
-1        'This is title'    'This is body'       3
-2        'other title'      'other body'         3
-3        'This is title'    'This is body'       2
+question_id     |      title      |       body        |  author_id
+1                    'This is title'    'This is body'       3
+2                   'other title'      'other body'         1
+3                   'This is title'    'This is body'       2
 
 
 question_follows
 
-id     |       user_id      |      question_id
-1         1                     1
-2         2                     1
+pair_id     |       follower_id      |      asked_question_id
+1                   1                     1
+2                   2                     1
+
+replies 
+
+reply_id |  body |  ref to question_id| parent_reply | replier_id(ref to user_id) |       
+1           "body"      1                0                   3               
+2           "body2"     1               ref to reply_id         1 
+3
+4
+5
+6                                          2  
+
+questions_likes
+
+likes_id|   user_id (ref to user_id)| question_id(ref to questions)      
+
+
+
+
+
+
 
 
