dd19d2a16cb8f7369d5578ca356a4024b0e55e23
atjohnfeng <atjohnfeng@gmail.com>
2021-08-05 17:29:41 -0700

    Added additional methods

--- w5d4/URLShortener/app/models/shortened_url.rb
+++ w5d4/URLShortener/app/models/shortened_url.rb
@@ -4,4 +4,9 @@ class ShortenedUrl < ApplicationRecord
     validates :long_url, presence: true
     validates :short_url, presence: true, uniqueness: true  
 
+    belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
 end 
--- w5d4/URLShortener/app/models/user.rb
+++ w5d4/URLShortener/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
 
+    has_many :short_urls,
+    primary_key: :id,
+    
+
 end
