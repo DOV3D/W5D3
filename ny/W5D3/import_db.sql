bee2f6e2abc026dbc8eb651ea5464a5c27407424
DOV3D <davidovelazquez3@gmail.com>
2021-09-08 12:16:16 -0400

    Add insert tables

--- ny/W5D3/import_db.sql
+++ ny/W5D3/import_db.sql
@@ -1,7 +1,14 @@
+PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname VARCHAR NOT NULL,
-    lname VARCHAR NOT NULL,
+    lname VARCHAR NOT NULL
 );
 
 CREATE TABLE questions(
@@ -25,7 +32,7 @@ CREATE TABLE question_follows(
 CREATE TABLE replies(
     id INTEGER PRIMARY KEY,
     reply_id INTEGER NOT NULL,
-    child_reply_id INTEGER NOT NULL,
+    child_reply_id INTEGER,
     author_id INTEGER NOT NULL,
     body TEXT NOT NULL,
 
@@ -46,6 +53,48 @@ CREATE TABLE question_likes(
 
 
 );
+
+INSERT INTO users
+    (fname, lname)
+VALUES 
+    ('John', 'Smith'),
+    ('James', 'Efron'),
+    ('Mike', 'Jones'),
+    ('Snigdha', 'Banda');
+
+INSERT INTO questions
+    (title, body, author_id)
+VALUES
+    ('Question 1', 'How long is App Academy?', 1),
+    ('Question 2', 'Where is Waldo?', 2),
+    ('Question 3', 'How hard is SQL?', 3);
+
+INSERT INTO question_follows
+    (follower_id, question_followed_id)
+VALUES
+    (1, 3),
+    (4, 2),
+    (3, 1);
+
+INSERT INTO replies
+    (reply_id, child_reply_id, author_id, body)
+VALUES 
+    (1,NULL, 2, "16 weeks"),
+    (2,NULL, 3, "Who knows?"),
+    (2, 1, 1, "Thats not a real answer");
+
+INSERT INTO question_likes
+    (liker_id, likes_id)
+VALUES
+    (1,1),
+    (2,2),
+    (4, 3),
+    (3, 1);
+
+
+
+
+
 
 
 
